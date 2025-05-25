c1de1179f570e3261834417dfd52ae8d50b6ed9d
Gaurav Rana <gaurav.rana14@gmail.com>
2025-05-25 23:02:23 +0530

    All YouTube clone files

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+import express from "express";
+import connectDB from "./db/index.js";
+const app = express();
+
+dotenv.config()
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runnig on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!! ", err);
+})
+
+/*
+(async () => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR", error);
+        throw error;
+    }
+})()
+*/
